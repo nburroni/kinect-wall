aecac0ce2dfa69847c8c2d31510181ac00459b7f
federuiz <ruizfederico95@gmail.com>
2016-06-10 16:20:18 -0300

    look at

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -14,9 +14,9 @@ public class Rotate : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		targetPoint = target.transform.position;
-		targetPoint.y = 0;
+		targetPoint.y = transform.position.y;
 		transform.LookAt(targetPoint);
-		Vector3 temp1 = new Vector3 (transform.position.x, transform.position.y + 0.1F, transform.position.z);
+		Vector3 temp1 = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 		transform.position = temp1;
 	}
 }
